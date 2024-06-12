3d0c245a4571b8f0717a36e37189c7aba8e4c9fd
Manuel Emilio Vargas Herrera <mjl778374@gmail.c>
2024-06-11 18:10:06 -0600

    Cambios a Scripts

--- InicioSesion.sh
+++ InicioSesion.sh
@@ -1,9 +1,3 @@
-if ! [[ "$PATH" =~ "$HOME/Scripts:" ]]
-then
-    PATH="$HOME/Scripts:$PATH"
-fi
-
-export PATH
 alias iniciarportforwarding="port-forwarding.sh"
 
 if [[ "$(ps aux | tr -s ' ' | cut -d ' ' -f11 | grep -c vivaldi-bin)" == "0" ]]
