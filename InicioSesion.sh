a1ad976da7c9424871aee236ab1edd2fabe57e8f
Manuel Emilio Vargas Herrera <mjl778374@gmail.c>
2024-06-29 14:59:06 -0600

    Cambios a Scripts

--- InicioSesion.sh
+++ InicioSesion.sh
@@ -1,9 +1,13 @@
+function IntentarIniciarApp {
+    if ! [[ -n $(ps aux | tr -s ' ' | cut -d ' ' -f11 | grep $1) ]]
+    then
+        echo "Iniciando $2..."
+        . "$3" &
+    fi
+} # function IntentarIniciarApp {
+
 alias iniciarportforwarding="port-forwarding.sh"
 
-if ! [[ -n "$(ps aux | tr -s ' ' | cut -d ' ' -f11 | grep vivaldi-bin)" ]]
-then
-    echo "Iniciando Vivaldi..."
-    Vivaldi.sh &
-fi
-
+IntentarIniciarApp "vivaldi-bin" "Vivaldi" "Vivaldi.sh"
 bijiben &
+anki &
