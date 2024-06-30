6171a5e1765a8d3522de03bc6bc00bb315f98e03
Manuel Emilio Vargas Herrera <mjl778374@gmail.c>
2024-06-30 17:55:57 -0600

    Cambios a Scripts

--- port-forwarding.sh
+++ port-forwarding.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
-
 systemctl start sshd
-ssh -L 8091:jorge:80 localhost
 
-#exit
+if [ $? -eq 0 ]; then
+    ssh -L 8091:jorge:80 localhost
+
+    if [ $? -eq 0 ]; then
+        exit 0
+    else
+        exit 2
+    fi
+else
+    exit 1
+fi
+
