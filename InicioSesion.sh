d390d617517163e377c0dfa46abb35df515523d2
Manuel Emilio Vargas Herrera <mjl778374@gmail.c>
2024-06-30 17:00:19 -0600

    Cambios a Scripts

--- InicioSesion.sh
+++ InicioSesion.sh
@@ -1,13 +1,22 @@
+carpeta_scripts="$HOME/Scripts"
+
+if ! [[ "$PATH" =~ "$carpeta_scripts:" ]]
+then
+    PATH="$carpeta_scripts:$PATH"
+fi
+
+export PATH
+
 function IntentarIniciarApp {
-    if ! [[ -n $(ps aux | tr -s ' ' | cut -d ' ' -f11 | grep $1) ]]
+    if ! [[ -n $(ps aux | tr -s ' ' | cut -d ' ' -f11 | grep --line-regexp $1) ]]
     then
         echo "Iniciando $2..."
-        . "$3" &
+        "$3" &
     fi
 } # function IntentarIniciarApp {
 
 alias iniciarportforwarding="port-forwarding.sh"
 
-IntentarIniciarApp "vivaldi-bin" "Vivaldi" "Vivaldi.sh"
-bijiben &
-anki &
+IntentarIniciarApp "/opt/vivaldi/vivaldi-bin" "Vivaldi" "Vivaldi.sh"
+IntentarIniciarApp "bijiben" "Notas" "bijiben"
+IntentarIniciarApp "anki" "Anki" "anki"
