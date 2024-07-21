13dc1a87407a481559aa17fad55e1a43f1207d94
Manuel Emilio Vargas Herrera <mjl778374@gmail.c>
2024-07-21 16:42:00 -0600

    Cambios a Scripts

--- LibroAmpliado.sh
+++ LibroAmpliado.sh
@@ -25,6 +25,8 @@ elif [ $1 == "11" ]; then
   nombre_archivo="Resumen Ampliado_ El arte de la seducción, de Robert Greene.pdf"
 elif [ $1 == "12" ]; then
   nombre_archivo="Resumen Ampliado_ Crea tu segundo cerebro, de Tiago Forte.pdf"
+elif [ $1 == "13" ]; then
+  nombre_archivo="Resumen Ampliado_ Prisioneros de la geografía, de Tim Marshall.pdf"
 else
   echo "El parámetro '$1' no corresponde a ningún libro ampliado."
   exit 2
