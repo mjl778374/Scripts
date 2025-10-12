f24a3c67df3f1dc99f006420ba180dade2c4e089
Manuel Emilio Vargas Herrera <mjl778374@gmail.c>
2025-10-12 17:44:53 -0600

    Cambios a Scripts

--- JavaUML.sh
+++ JavaUML.sh
@@ -12,7 +12,7 @@ ruta_archivo="/run/media/$usuario/Java y UML/inicio.htm"
 ls "${ruta_archivo}" > /dev/null
 
 if ! [ $? -eq 0 ]; then
-  echo "No se puede acceder o no existe el archivo ${ruta_archivo}"
+  echo "No existe el archivo '${ruta_archivo}' o no se puede acceder a él"
   exit 2
 fi
 
